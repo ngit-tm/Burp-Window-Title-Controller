26c13e24fc64565a284ebc69d227dd58b2846ece
ronen1n <Ronenltd@gmail.com>
2024-12-23 14:59:12 +0200

    Removed the time func to check for title

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -5,8 +5,6 @@ import burp.api.montoya.MontoyaApi;
 import javax.swing.*;
 
 public class BurpExtender implements BurpExtension {
-    private Timer titleCheckTimer;
-
     @Override
     public void initialize(MontoyaApi api) {
         api.extension().setName("Burp License Title Cleaner");
@@ -14,18 +12,12 @@ public class BurpExtender implements BurpExtension {
         // Get the main Burp frame using the recommended method
         JFrame burpFrame = (JFrame) api.userInterface().swingUtils().suiteFrame();
         
-        // Create a timer to periodically check and update the title
-        titleCheckTimer = new Timer(1000, _ -> {
-            String currentTitle = burpFrame.getTitle();
-            if (currentTitle.contains(" - licensed to ")) {
-                // Remove the license information and set the clean title
-                String cleanTitle = currentTitle.replaceFirst(" - licensed to .*", "");
-                burpFrame.setTitle(cleanTitle);
-            }
-        });
-        
-        // Start the timer
-        titleCheckTimer.start();
+        // Clean the title immediately
+        String currentTitle = burpFrame.getTitle();
+        if (currentTitle.contains(" - licensed to ")) {
+            String cleanTitle = currentTitle.replaceFirst(" - licensed to .*", "");
+            burpFrame.setTitle(cleanTitle);
+        }
         
         // Log extension loaded
         api.logging().logToOutput("Burp License Title Cleaner extension loaded");
